d337c47dbfce0dd79281642c67d208bb4100edc5
n1215 <knakae1215@gmail.com>
2022-03-26 13:19:06 +0900

    DI testing for middleware and console command by overriding Application class

---
+++ app/Console/Commands/HelloCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Domain\Double\DoubleInterface;
+use Illuminate\Console\Command;
+
+class HelloCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hello';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Hello';
+
+    /**
+     * @param string $description
+     */
+    public function __construct(private readonly DoubleInterface $double) {
+        parent::__construct();
+    }
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $doubled = $this->double->double(3);
+        $this->line("Hello {$doubled} from a console command");
+        return 0;
+    }
+}
---
+++ app/Http/Middleware/HiMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Domain\Double\DoubleInterface;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HiMiddleware
+{
+
+    public function __construct(private readonly DoubleInterface $double)
+    {
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var Response $response */
+        $response = $next($request);
+
+        $doubled = $this->double->double(2);
+        $response->setContent($response->getContent() . "<br> Hi {$doubled} from a middleware");
+
+        return $response;
+    }
+}
